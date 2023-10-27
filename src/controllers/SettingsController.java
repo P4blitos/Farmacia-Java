608c6b5079d81893cdc6c4938478c02e536e944a
P4blitos <pablosernacardenas@gmail.com>
2023-10-27 09:39:41 -0500

    Initial commit

---
+++ src/controllers/SettingsController.java
@@ -0,0 +1,82 @@
+package controllers;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import views.SystemView;
+
+public class SettingsController implements MouseListener{
+    
+    private SystemView views;
+    //constructor
+    public SettingsController(SystemView views){
+        this.views= views;
+        this.views.jLabelProducts.addMouseListener(this);
+        this.views.jLabelPurchases.addMouseListener(this);
+        this.views.jLabelCustomers.addMouseListener(this);
+        this.views.jLabelEmployees.addMouseListener(this);
+        this.views.jLabelSuppliers.addMouseListener(this);
+        this.views.jLabelCategories.addMouseListener(this);
+        this.views.jLabelReports.addMouseListener(this);
+        this.views.jLabelSettings.addMouseListener(this);
+    }
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+    //Metodo de estar encima del label
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        if(e.getSource()== views.jLabelProducts){
+            views.JpanelProducts.setBackground(new Color(200,67,63));
+        }else if(e.getSource()== views.jLabelPurchases){
+            views.jPanelPurchases.setBackground(new Color(200,67,63));
+        }else if(e.getSource()==views.jLabelCustomers){
+            views.jPanelCustomers.setBackground(new Color(200,67,63));
+        }else if(e.getSource()== views.jLabelEmployees){
+            views.jPanelEmployees.setBackground(new Color(200,67,63));
+        }else if(e.getSource()== views.jLabelSuppliers){
+            views.jPanelSuppliers.setBackground(new Color(200,67,63));
+        }else if(e.getSource()== views.jLabelCategories){
+            views.jPanelCategories.setBackground(new Color(200,67,63));
+        }else if(e.getSource()== views.jLabelReports){
+            views.jPanelReports.setBackground(new Color(200,67,63));
+        }else if(e.getSource()== views.jLabelSettings){
+            views.jPanelSettings.setBackground(new Color(200,67,63));
+        }
+    }
+    
+    //metodo de salida del Label
+    @Override
+    public void mouseExited(MouseEvent e) {
+         if(e.getSource()== views.jLabelProducts){
+            views.JpanelProducts.setBackground(new Color(34,121,173));
+        }else if(e.getSource()== views.jLabelPurchases){
+            views.jPanelPurchases.setBackground(new Color(34,121,173));
+        }else if(e.getSource()==views.jLabelCustomers){
+            views.jPanelCustomers.setBackground(new Color(34,121,173));
+        }else if(e.getSource()== views.jLabelEmployees){
+            views.jPanelEmployees.setBackground(new Color(34,121,173));
+        }else if(e.getSource()== views.jLabelSuppliers){
+            views.jPanelSuppliers.setBackground(new Color(34,121,173));
+        }else if(e.getSource()== views.jLabelCategories){
+            views.jPanelCategories.setBackground(new Color(34,121,173));
+        }else if(e.getSource()== views.jLabelReports){
+            views.jPanelReports.setBackground(new Color(34,121,173));
+        }else if(e.getSource()== views.jLabelSettings){
+            views.jPanelSettings.setBackground(new Color(34,121,173));
+        }
+
+    }
+    
+}
---
+++ src/main/main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import views.LoginView;
+
+/**
+ *
+ * @author Equipo
+ */
+public class main {
+    public static void main(String[] args) {
+        //instancia del login
+        LoginView loginview = new LoginView();
+        loginview.setVisible(true);
+    }
+}
